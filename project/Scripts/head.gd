8c15204ef15284e8d877a0fe7581c38a658c3d52
Beau <willohsbudda@gmail.com>
2026-04-13 18:58:04 -0400

    PlayerMovement/Camera

--- project/Scripts/head.gd
+++ project/Scripts/head.gd
@@ -1,11 +1,13 @@
 extends Node3D
 
+var sensitivity = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		get_parent().rotate_y(deg_to_rad(-event.relative.x * sensitivity))
+		rotate_x(deg_to_rad(-event.relative.y))
+		rotation.x = clampf(rotation.x, deg_to_rad(-90), deg_to_rad(90))
