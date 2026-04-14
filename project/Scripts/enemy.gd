3e4126bbd5fc5ebdefc7ac0453c9927f690c0e54
Just1n-Clark <justinclark001@icloud.com>
2026-04-13 19:27:58 -0500

    enemy follow player + prototype first levevl

--- project/Scripts/enemy.gd
+++ project/Scripts/enemy.gd
@@ -3,34 +3,31 @@ extends CharacterBody3D
 
 #region References
 @onready var nav_agent = $NavigationAgent3D
+@export var player: Player
 #endregion
 
 var health: float
 const MAX_HEALTH: float = 20
 
-var speed: float = 3
-var direction: Vector3
+const GRAVITY = 150
+var speed: float = 2
 
 func _ready() -> void:
 	health = MAX_HEALTH
 	
 func _physics_process(delta: float) -> void:
+	assert(player, "Player not set in inpector")
+	nav_agent.target_position = player.global_position
+	#print(nav_agent.target_position)
+	
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()
+	#print(next_location, " ", current_location)
 	var new_velocity = (next_location - current_location).normalized() * speed
 	
-	velocity = new_velocity + direction * speed
+	velocity = new_velocity * speed
 	
 	if not is_on_floor():
-		velocity.y -= 300 * delta
+		velocity.y -= GRAVITY * delta
 	
 	move_and_slide()
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventMouseMotion:
-		var mouse_pos_2d = get_viewport().get_mouse_position()
-		var camera = get_viewport().get_camera_3d()
-		
-		var mouse_pos_3d: Vector3 = camera.project_position(mouse_pos_2d, 0)
-		
-		nav_agent.target_position = mouse_pos_3d
